f73361f53a1b115515478205c988c1c67dd912a6
Oro1st <79486412+Oro1st@users.noreply.github.com>
2021-03-23 20:38:28 -0500

    W1D2_Home_Work

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,24 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// See tests.js for behavior
+function nameString(obj) {
+    if (obj === null) {
+        return "null argument";
+    }
+    if (obj === undefined || obj === null) {
+        return "no object passed";
+    }
+    if (obj.length === 0) {
+        return "object has no properties";
+    }
+    let arr = [];
+    for (let k in obj) {
+        arr.push(k)
+    }
+    arr.sort();
+
+    let str = arr.join('.')
+    return str
 }
